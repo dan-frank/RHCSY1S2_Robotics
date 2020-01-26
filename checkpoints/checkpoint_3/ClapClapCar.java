45e717d7c888ee1531a75298570b316aa41d6c11
lblockrhul <lewes.block.2019@live.rhul.ac.uk>
2020-01-26 12:37:30 +0000

    Started ClapClapCar.java; removed temp

---
+++ checkpoints/checkpoint_3/ClapClapCar.java
@@ -0,0 +1,17 @@
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.NXTSoundSensor;
+import lejos.robotics.SampleProvider;
+
+public class ClapClapCar {
+
+	public static void main(String[] args) {
+		float[] level = new float [1]; // A sound sample is just one number
+		NXTSoundSensor ss = new NXTSoundSensor(SensorPort.S1);
+		SampleProvider sound = ss.getDBAMode();
+		
+		while(true) {
+			
+		}
+	}
+
+}
