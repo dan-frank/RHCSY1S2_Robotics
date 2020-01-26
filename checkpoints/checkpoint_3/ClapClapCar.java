6efcf2024589d5558c0fba5ad9f8d1c4b5f59bd5
dan-frank <dan.frank.lucas@gmail.com>
2020-01-26 13:03:33 +0000

    Prints soundlevel to LCD

--- checkpoints/checkpoint_3/ClapClapCar.java
+++ checkpoints/checkpoint_3/ClapClapCar.java
@@ -1,5 +1,6 @@
 package checkpoints.checkpoint_3;
 
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.NXTSoundSensor;
 import lejos.robotics.SampleProvider;
@@ -7,12 +8,19 @@ import lejos.robotics.SampleProvider;
 public class ClapClapCar {
 
 	public static void main(String[] args) {
-		float[] level = new float [1]; // A sound sample is just one number
+		float[] level = new float[1]; // A sound sample is just one number
 		NXTSoundSensor ss = new NXTSoundSensor(SensorPort.S1);
 		SampleProvider sound = ss.getDBAMode();
 		
+		String soundLevel = "";
+		
 		while(true) {
+			sound.fetchSample(level, 0);
 			
+			soundLevel = String.valueOf(level[0]);
+			
+			LCD.clear();
+			LCD.drawString(soundLevel, 2 , 2);
 		}
 	}
 
