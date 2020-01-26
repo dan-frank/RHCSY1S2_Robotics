d6267d3d722aa88f6c0103e9565b321b73a51b5b
megan <megan@Megans-MacBook-Air-3.local>
2020-01-26 15:53:09 +0000

    added turn and distance checker

--- checkpoints/checkpoint_3/SimpleChap.java
+++ checkpoints/checkpoint_3/SimpleChap.java
@@ -7,6 +7,7 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.NXTSoundSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
@@ -19,23 +20,41 @@ public class SimpleChap {
 		SensorMode sound = (SensorMode) sensor.getDBAMode();
 		SampleProvider clap = new ClapFilter(sound, 0.6f, 100);
 		
+		float[] level2 = new float[1];
+		EV3UltrasonicSensor sensor2 = new EV3UltrasonicSensor(SensorPort.S1);
+		SensorMode distance = (SensorMode) sensor2.getDistanceMode();
+		
+		
 		BaseRegulatedMotor mLeft = new EV3LargeRegulatedMotor ( MotorPort.A );
 		BaseRegulatedMotor mRight = new EV3LargeRegulatedMotor ( MotorPort.B );
 		mLeft.setSpeed(360); // 1 Revolution Per Second ( RPS )
 		mRight.setSpeed(360);
 		mLeft.synchronizeWith( new BaseRegulatedMotor[] { mRight });
 		
+		boolean count = true;
+		distance.fetchSample(level2, 0);
+		
 		// Follows the line
-		while(!Button.ENTER.isDown()) {
+		while(level[0] > 0.50) {
 			// Stores light into level[0]
 			clap.fetchSample(level, 0);
 			
 			mLeft.startSynchronization();
 			if (level[0] == 1.0f) {
-				mLeft.forward();
-				mRight.forward();
+				if (count) {
+					mLeft.forward();
+					mRight.forward();
+					count = false;
+				} else {
+					mRight.stop();
+					mLeft.stop();
+					mLeft.rotate(707);
+					Delay.msDelay(500);
+					count = true;
+				}
 			}
 			mLeft.endSynchronization();
+			distance.fetchSample(level2, 0);
 		}
 		
 		sensor.close();
