de98522e11b4618be1e809938f826a87df823d50
dan-frank <dan.frank.lucas@gmail.com>
2020-01-31 11:56:53 +0000

    Fixed compiler errors

--- checkpoints/checkpoint_4/PilotSquare.java
+++ checkpoints/checkpoint_4/PilotSquare.java
@@ -7,12 +7,19 @@ import lejos.hardware.port.MotorPort;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.geometry.Line;
+import lejos.robotics.geometry.Rectangle;
 import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.localization.PoseProvider;
+import lejos.robotics.mapping.LineMap;
+import lejos.robotics.navigation.DestinationUnreachableException;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.navigation.Navigator;
+import lejos.robotics.navigation.Pose;
 import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.pathfinding.Path;
+import lejos.robotics.pathfinding.PathFinder;
+import lejos.robotics.pathfinding.ShortestPathFinder;
 
 public class PilotSquare {
 	final static float WHEELDIAMETER = 56; // The diameter (mm) of the wheels
@@ -23,12 +30,12 @@ public class PilotSquare {
 	
 	public static void main(String[] args) {
 // Wheel Diameter 60mm, both wheels set 29mm from car centre
-		BaseRegulatedMotor mLeft = new EV3LargeRegulatedMotor(MotorPort.A);
-		BaseRegulatedMotor mRight = new EV3LargeRegulatedMotor(MotorPort.B);
+		BaseRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.A);
+		BaseRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.B);
 	
 		Wheel wL = WheeledChassis.modelWheel(motorL, WHEELDIAMETER).offset(-AXLELENGTH / 2);
 		
-		Wheel wR = WheeledChassis.modelWheel(motorL, WHEELDIAMETER).offset(AXLELENGTH / 2);
+		Wheel wR = WheeledChassis.modelWheel(motorR, WHEELDIAMETER).offset(AXLELENGTH / 2);
 		Wheel[] ws = new Wheel[] { wR, wL };
 		Chassis chassis = new WheeledChassis(ws, WheeledChassis.TYPE_DIFFERENTIAL);
 		MovePilot plt = new MovePilot(chassis);
@@ -37,25 +44,31 @@ public class PilotSquare {
 		Navigator navigator = new Navigator(plt, poseProvider);
 		
 		Line [] lines = new Line[4];
-		lines[0]= new Line (210,0,210, 620);
-		lines[1]= new Line (210, 620, 380,620);
-		lines[2]= new Line (380,620, 380, 0);
-		lines[3]= new Line (210,0,380,0);
+		lines[0] = new Line (210,0,210, 620);
+		lines[1] = new Line (210, 620, 380,620);
+		lines[2] = new Line (380,620, 380, 0);
+		lines[3] = new Line (210,0,380,0);
 		Rectangle bounds = new Rectangle (-50,-50, 800,600);
-		|
-		LineMap map = new LineMap (lines,bounds);
+		
+		LineMap map = new LineMap(lines, bounds);
 		PathFinder pf = new ShortestPathFinder(map);
-		Path path =pf.findRoute(new Pose(130,150,90),new Waypoint(480,200));
-		navigator.followPath(path);
+		
+		try {
+			Path path = pf.findRoute(new Pose(130,150,90), new Waypoint(480,200));
+			navigator.followPath(path);
+		} catch (DestinationUnreachableException e) {
+			LCD.clear();
+			LCD.drawString("Cannot reach", 1, 1);
+			LCD.drawString("destinations", 1, 2);
+		}
 		
 		navigator.waitForStop();
-
 
 		LCD.clear();
 
 		LCD.drawString("Path Finished!", 2, 2);
-		mLeft.close();
-		mRight.close();
+		motorL.close();
+		motorR.close();
 	}
 
 }
