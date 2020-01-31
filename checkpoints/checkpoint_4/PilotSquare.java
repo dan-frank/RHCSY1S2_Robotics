465a5fdd6dcda02063926003f03ca0026a02c648
dan-frank <dan.frank.lucas@gmail.com>
2020-01-31 12:13:27 +0000

    Swapped x & y axis on second bounds

--- checkpoints/checkpoint_4/PilotSquare.java
+++ checkpoints/checkpoint_4/PilotSquare.java
@@ -20,6 +20,7 @@ import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.pathfinding.Path;
 import lejos.robotics.pathfinding.PathFinder;
 import lejos.robotics.pathfinding.ShortestPathFinder;
+import lejos.utility.Delay;
 
 public class PilotSquare {
 	final static float WHEELDIAMETER = 56; // The diameter (mm) of the wheels
@@ -29,14 +30,14 @@ public class PilotSquare {
 	final static float LINEARSPEED = 70; // How fast in a straight line (mm/sec)
 	
 	public static void main(String[] args) {
-// Wheel Diameter 60mm, both wheels set 29mm from car centre
+		// Wheel Diameter 60mm, both wheels set 29mm from car centre
 		BaseRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.A);
 		BaseRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.B);
 	
 		Wheel wL = WheeledChassis.modelWheel(motorL, WHEELDIAMETER).offset(-AXLELENGTH / 2);
-		
 		Wheel wR = WheeledChassis.modelWheel(motorR, WHEELDIAMETER).offset(AXLELENGTH / 2);
-		Wheel[] ws = new Wheel[] { wR, wL };
+		Wheel[] ws = new Wheel[] {wR, wL};
+		
 		Chassis chassis = new WheeledChassis(ws, WheeledChassis.TYPE_DIFFERENTIAL);
 		MovePilot plt = new MovePilot(chassis);
 	
@@ -44,17 +45,17 @@ public class PilotSquare {
 		Navigator navigator = new Navigator(plt, poseProvider);
 		
 		Line [] lines = new Line[4];
-		lines[0] = new Line (210,0,210, 620);
-		lines[1] = new Line (210, 620, 380,620);
-		lines[2] = new Line (380,620, 380, 0);
-		lines[3] = new Line (210,0,380,0);
-		Rectangle bounds = new Rectangle (-50,-50, 800,600);
+		lines[0] = new Line (210, 0, 210, 620);
+		lines[1] = new Line (210, 620, 380, 620);
+		lines[2] = new Line (380, 620, 380, 0);
+		lines[3] = new Line (210, 0, 380, 0);
+		Rectangle bounds = new Rectangle (-50, -50, 600, 800);
 		
 		LineMap map = new LineMap(lines, bounds);
 		PathFinder pf = new ShortestPathFinder(map);
 		
 		try {
-			Path path = pf.findRoute(new Pose(130,150,90), new Waypoint(480,200));
+			Path path = pf.findRoute(new Pose(130, 150, 90), new Waypoint(480, 200));
 			navigator.followPath(path);
 		} catch (DestinationUnreachableException e) {
 			LCD.clear();
@@ -65,8 +66,10 @@ public class PilotSquare {
 		navigator.waitForStop();
 
 		LCD.clear();
-
 		LCD.drawString("Path Finished!", 2, 2);
+		
+		Delay.msDelay(2000);
+		
 		motorL.close();
 		motorR.close();
 	}
