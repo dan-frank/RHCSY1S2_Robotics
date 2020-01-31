7c5aa86a859f17635d3e31d86edcfc64cc5f34b5
Yonkov <ZHAC360@live.rhul.ac.uk>
2020-01-31 11:22:50 +0000

    Added proper WheelDiameter and AxelLength

--- checkpoints/checkpoint_4/PilotSquare.java
+++ checkpoints/checkpoint_4/PilotSquare.java
@@ -15,33 +15,35 @@ import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.pathfinding.Path;
 
 public class PilotSquare {
+	final static float WHEELDIAMETER = 56; // The diameter (mm) of the wheels
+	final static float WHEELTHICKNESS = 26; // The diameter (mm) of the wheels
+	final static float AXLELENGTH = 147 - WHEELTHICKNESS; // The distance (mm) your two driven wheels
+	final static float ANGULARSPEED = 100; // How fast around corners (degrees/sec)
+	final static float LINEARSPEED = 70; // How fast in a straight line (mm/sec)
 	
 	public static void main(String[] args) {
 // Wheel Diameter 60mm, both wheels set 29mm from car centre
 		BaseRegulatedMotor mLeft = new EV3LargeRegulatedMotor(MotorPort.A);
 		BaseRegulatedMotor mRight = new EV3LargeRegulatedMotor(MotorPort.B);
-		Wheel wL = WheeledChassis.modelWheel(mLeft, 56).offset(-73.5);
-
-		Wheel wR = WheeledChassis.modelWheel(mRight, 56).offset(73.5);
+	
+		Wheel wL = WheeledChassis.modelWheel(motorL, WHEELDIAMETER).offset(-AXLELENGTH / 2);
+		
+		Wheel wR = WheeledChassis.modelWheel(motorL, WHEELDIAMETER).offset(AXLELENGTH / 2);
 		Wheel[] ws = new Wheel[] { wR, wL };
 		Chassis chassis = new WheeledChassis(ws, WheeledChassis.TYPE_DIFFERENTIAL);
 		MovePilot plt = new MovePilot(chassis);
-		//plt.setAngularSpeed(200);
-		//plt.setLinearSpeed(360);
+	
 		PoseProvider poseProvider = new OdometryPoseProvider(plt);
 		Navigator navigator = new Navigator(plt, poseProvider);
 		Path path = new Path();
-		path.add(new Waypoint(100, 0));
-		path.add(new Waypoint(100, 100));
-		path.add(new Waypoint(0, 100));
+		path.add(new Waypoint(50, 0));
+		path.add(new Waypoint(50, 50));
+		path.add(new Waypoint(0, 50));
 		path.add(new Waypoint(0, 0));
 		navigator.followPath(path);
 		navigator.waitForStop();
 
-//		for (int side = 0; side < 4; side++) {
-//			plt.travel(300);
-//			plt.rotate(90);
-//		}
+
 		LCD.clear();
 
 		LCD.drawString("Path Finished!", 2, 2);
