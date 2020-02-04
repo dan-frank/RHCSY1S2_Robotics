cb8c7556fd845248525ce96d102dfcbbb460bccc
dan-frank <dan.frank.lucas@gmail.com>
2020-02-04 11:40:09 +0000

    checkpoint5

--- checkpoints/checkpoint_5/Trundle.java
+++ checkpoints/checkpoint_5/Trundle.java
@@ -1,6 +1,6 @@
 package checkpoints.checkpoint_5;
 
-public class Behavior {
+public class Trundle implements Behavior {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
