aee1bfcd5657dc2638b73c69748c069fc8122b59
lblockrhul <lewes.block.2019@live.rhul.ac.uk>
2020-02-07 16:03:21 +0000

    started Bluetooth.java

---
+++ checkpoints/checkpoint_5/Bluetooth.java
@@ -0,0 +1,13 @@
+package checkpoints.checkpoint_5;
+
+import lejos.robotics.subsumption.Behavior;
+
+public class Bluetooth {
+	public boolean takeControl(){
+		return();	//returns true if a bluetooth message is available
+	}
+	
+	public void action(){}
+	
+	public void suppress(){}
+}
