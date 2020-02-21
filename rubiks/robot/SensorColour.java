61524ced9c4a285309af666454b607f15614b956
dan-frank <dan.frank.lucas@gmail.com>
2020-02-21 14:06:13 +0000

    Updated sensor to use RedMode()

--- rubiks/robot/SensorColour.java
+++ rubiks/robot/SensorColour.java
@@ -1,19 +1,21 @@
 package rubiks.robot;
 
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
 
 public class SensorColour extends RubeDaddy {
-	
 	private EV3ColorSensor s;
+	private SampleProvider mode;
 	private float[] measurement;
 	
 	public SensorColour() {
 		this.s = sensorPortColour;
+		this.mode = this.s.getRedMode();
 		this.measurement = new float[1];
 	}
 	
 	public float scan() {
-		s.fetchSample(measurement, 0);
+		mode.fetchSample(measurement, 0);
 		return measurement[0];
 	}
 }
