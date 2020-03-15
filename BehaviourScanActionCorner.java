5cced24e4a3c5ede574eb8eb0e90bb43160ca4fa
lblockrhul <lewes.block.2019@live.rhul.ac.uk>
2020-03-15 23:44:33 +0000

    Added BehaviourScanActionCorner
    as it was empty before

--- BehaviourScanActionCorner.java
+++ BehaviourScanActionCorner.java
@@ -1,4 +1,30 @@
+import lejos.robotics.subsumption.Behavior;
 
-public class BehaviourScanActionCorner {
+public class BehaviourScanActionCorner implements Behavior {
+	private FriendScan friendScan;
+	private MotorColour motorColour;
+
+	public BehaviourScanActionCorner(FriendScan friendScan, MotorColour motorColour) {
+		this.friendScan = friendScan;
+		this.motorColour = motorColour;
+
+	}
+
+	@Override
+	public boolean takeControl() {
+		return friendScan.getStateMoveColourMotor()==StateMoveColourMotor.CORNER;
+	}
+
+	@Override
+	public void action() {
+	     motorColour.run(StateMoveColourMotor.CORNER);
+
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
